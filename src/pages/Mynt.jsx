a06a59a38ddeb8d6069538d661ffe5dc4d45b433
sakshi-pyvot <sakshi@pyvot.in>
2026-08-25 12:34:09 +0530

    Retake all 7 dashboard screenshots with clean uniform margins; bump shot cache to v6

--- src/pages/Mynt.jsx
+++ src/pages/Mynt.jsx
@@ -422,7 +422,7 @@ function DashboardShowcase() {
                 style={i > 0 ? { clipPath: 'inset(100% 0 0 0)' } : undefined}
               >
                 <img
-                  src={`/shots/${x.shot}.jpg?v=5`}
+                  src={`/shots/${x.shot}.jpg?v=6`}
                   alt={`${x.label} dashboard`}
                   className="h-full w-full object-contain"
                 />
@@ -484,7 +484,7 @@ function DashboardShowcase() {
               transition={{ duration: 0.25 }}
             >
               <img
-                src={`/shots/${d.shot}.jpg?v=5`}
+                src={`/shots/${d.shot}.jpg?v=6`}
                 alt={`${d.label} dashboard`}
                 className="rounded-xl border border-line"
               />
